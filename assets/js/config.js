55a01a7f77114a0a375698c56025631d24fbd1ad
Sabika Saba <sabikhtsabaa@gmail.com>
2026-08-21 02:07:00 +0300

    Publish Sabika Saba 1.0.0

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
-﻿window.SABIKA_SITE_CONFIG = {
-  version: "7",
+window.SABIKA_SITE_CONFIG = {
+  version: "1.0.0",
   downloadUrl: "https://github.com/sabikhtsabaa-sketch/sabika-saba-site/releases/latest/download/Sabika-Saba.apk",
   downloadFileName: "Sabika-Saba.apk"
 };
